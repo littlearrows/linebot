aa64d4874668e5cdd6dd1b072a8a6f5b4ba8999c
littlearrows <littlearrownexus@gmail.com>
2019-06-13 15:30:39 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,3 @@
-<?php
-echo "Hello LINE bot World!";
-?>
+
+<?php /* Redirect browser */ header("Location:http://780f069531c5.sn.mynetname.net:7208/notiamount/line_notify.php"); /* Make sure that code below does not get executed when we redirect. */ exit; ?>
+
